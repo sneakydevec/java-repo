aee90a1c99180bbb1fabe9f77eb4338c7127fde1
Verpak <mrverpak@gmail.com>
2022-07-23 01:05:43 +0200

    New code

--- src/test/java/com/api/arctindev/ArctindevApplicationTests.java
+++ src/test/java/com/api/arctindev/ArctindevApplicationTests.java
@@ -3,11 +3,18 @@ package com.api.arctindev;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.api.arctindev.model.Article;
+
 @SpringBootTest
 class ArctindevApplicationTests {
 
+	Article article;
 	@Test
 	void contextLoads() {
+
+		Article article = new Article();
+
+		article.getAuthor().equals("anObject");
 	}
 
 }
